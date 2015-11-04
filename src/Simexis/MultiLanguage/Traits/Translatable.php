8a38781f23702e9800d9676819a24b7c55c2094e
Georgi Nachev <g.nachev@webmedia.bg>
2015-11-04 17:44:29 +0200

    Optimase multiple database get data

--- src/Simexis/MultiLanguage/Traits/Translatable.php
+++ src/Simexis/MultiLanguage/Traits/Translatable.php
@@ -356,6 +356,38 @@ trait Translatable
     }
 
     /**
+     * Handle dynamic method calls into the model.
+     *
+     * @param  string  $method
+     * @param  array   $parameters
+     * @return mixed
+     */
+    public function __call($method, $parameters)
+    {
+        if (in_array($method, ['increment', 'decrement'])) {
+            return call_user_func_array([$this, $method], $parameters);
+        }
+
+        $query = $this->newQuery()->with('translations');
+
+        return call_user_func_array([$query, $method], $parameters);
+    }
+
+    /**
+     * Handle dynamic static method calls into the method.
+     *
+     * @param  string  $method
+     * @param  array   $parameters
+     * @return mixed
+     */
+    public static function __callStatic($method, $parameters)
+    {
+        $instance = new static;
+
+        return call_user_func_array([$instance->with('translations'), $method], $parameters);
+    }
+
+    /**
      * @param \Illuminate\Database\Eloquent\Builder $query
      * @param string $locale
      *
